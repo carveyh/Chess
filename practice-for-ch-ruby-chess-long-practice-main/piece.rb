4f90f9626877a907d73cf584f225328ac39eb772
Ethanjonm <ethanjonmercado@gmail.com>
2023-03-28 16:32:47 -0400

    Piece

--- practice-for-ch-ruby-chess-long-practice-main/piece.rb
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -1,4 +1,19 @@
 class Piece
-	def initialize
+	def initialize(color, board, pos)
+		@color = color
+		@board = board
+		@pos = pos
+	end
+
+	def pos=(new_pos)
+		@pos = new_pos
+	end
+
+	def symbol
+		@color
+	end
+
+	def move_into_check?(end_pos)
+		#check valid moves if king piece is there
 	end
 end

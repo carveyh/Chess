12d8b57c093657acfb600b193ac3b311053fcf29
Ethanjonm <ethanjonmercado@gmail.com>
2023-03-28 15:10:25 -0400

    populate board

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,9 +1,20 @@
+require_relative "piece.rb"
+
+
 class Board
 
 	def initialize
 		@board = Array.new(8) { Array.new(8) }
-		
+		populate_board
 	end
+
+    def populate_board
+        [0,1,6,7].each do |row_idx|
+            @board[row_idx].map! do |space|
+                Piece.new
+            end
+        end
+    end
     
 	def [](pos) #pos == [x, y]
 		a, b = pos

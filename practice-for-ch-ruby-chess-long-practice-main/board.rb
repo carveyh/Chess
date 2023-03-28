812e6a9f4b4ad7be5c7fd6c1c9f28bc355a37ef3
carveyh <carvey.hor@gmail.com>
2023-03-28 14:59:30 -0400

    Add #initialize, #[], #[]=

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,5 +1,21 @@
-class board
+class Board
 
+	def initialize
+		@board = Array.new(8) { Array.new(8) }
+		
+	end
     
+	def [](pos) #pos == [x, y]
+		a, b = pos
+		@board[a][b] # @board[x,y]
+	end
+
+	def []=(pos, val)
+		a, b = pos
+		@board[a][b] = val
+	end
+
+	def move_piece(start_pos, end_pos)
+	end
 
 end
